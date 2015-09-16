86174713a07d3af64b4715e2014ded87b75be21e
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-09-15 21:02:27 -0300

    [WIP] Creating board

--- src/assets/javascripts/components/board.js
+++ src/assets/javascripts/components/board.js
@@ -1,54 +1,21 @@
 import React from 'react';
+import Field from './field';
 
-export default class App extends React.Component {
+export default class Board extends React.Component {
 
   constructor(props) {
     super(props);
   }
 
-  static defaultProps = {
-    description: 'This is a default prop value.',
-  }
-
-  state = {
-    description: this.props.description
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    var description = this.getDescriptionValue();
-    this.setState({
-      description: description ? description : this.props.description
-    });
-  }
-
-  getDescriptionValue = () => {
-    return this.refs.description.getDOMNode().value;
-  }
-
-  generate = () => {
-
-  }
-
   render = () => {
     return(
-      <table className="c-board">
-        <tbody>
-          {
-            Array.apply(null, { length: 10 }).map(() => {
-              return (
-                <tr className="c-board__line">
-                  {
-                    Array.apply(null, { length: 10 }).map(() => {
-                      return <td className="c-board__item"></td>
-                    })
-                  }
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
+      <div className="c-board">
+        {
+          Array.apply(null, { length: 5 }).map(() => {
+            return (<Field />)
+          })
+        }
+      </div>
     )
   }
 }
