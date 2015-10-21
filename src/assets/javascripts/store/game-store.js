27c6bc49c7087fe46da340ae09bdc535c657b2bd
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-10-21 13:51:44 -0200

    fix(store): Removing old stuff

--- src/assets/javascripts/store/game-store.js
+++ src/assets/javascripts/store/game-store.js
@@ -8,15 +8,32 @@ import fieldsStore from './fields';
 let store = {game: true};
 let gameSubject = new Rx.BehaviorSubject(store);
 
+function isGameOver(user) {
+  return user.status.lose;
+}
+
+
+function isMatch(fields, id) {
+  return fields.filter((field) => asdf) === id;
+}
+
+// multiple actions
+// shared utils functions
+
+
 matchActions.subjects.trial.subscribe((id) => {
 
   let { status } = userStore.subject.getValue();
-  if (status.lose) {
+  if (isGameOver()) {
     return;
   }
 
   gameSubject.onNext(store);
 });
+
+window.fieldsStore = fieldsStore;
+window.userStore = userStore;
+window.matchActions = matchActions;
 
 export default Rx.Observable.combineLatest(
     gameSubject,
@@ -24,67 +41,3 @@ export default Rx.Observable.combineLatest(
     fieldsStore.subject,
     (...data) => data.reduce((c, x) => c = _.extend(c, x), {}));
 
-// export default new class GameStore extends Events {
-//
-//   constructor() {
-//     super();
-//     this.data = this.getStoreData();
-//     this.bind();
-//   }
-//
-//   data = {}
-//
-//   bind = () => {
-//       appDispatcher.register((action) => {
-//         switch (action.actionType) {
-//           case 'TRIAL': this.actionHandler(this.onActionTrial, action); break;
-//           case 'START': this.actionHandler(this.onActionStart, action); break;
-//         }
-//       });
-//   }
-//
-//   prepare = () => {
-//     timerStore.stop();
-//     this.data = this.getStoreData();
-//     fieldsStore.rebuild();
-//   }
-//
-//   getStoreData = () => {
-//     return _.extend(fieldsStore.data, timerStore.data, userStore.data);
-//   }
-//
-//   actionHandler = (fn, action) => {
-//     fn(action);
-//     this.emitChange();
-//   }
-//
-//   onActionStart = (action) => {
-//     this.prepare();
-//     userStore.start();
-//     timerStore.start(this.emitChange);
-//   }
-//
-//   onActionTrial = (action) => {
-//     if (userStore.isGameOver()) {
-//       return;
-//     }
-//
-//     if (fieldsStore.isMatched(action.id)) {
-//       fieldsStore.setMatched();
-//       userStore.isWinner() && userStore.setWinner();
-//       timerStore.countdown.add(2);
-//       return;
-//     }
-//
-//     timerStore.countdown.decrease(10);
-//   }
-//
-//   addChangeListener = (callback) => {
-//     this.on('change', callback);
-//   }
-//
-//   emitChange = () => {
-//     this.data = this.getStoreData();
-//     this.emit('change');
-//   }
-// }
