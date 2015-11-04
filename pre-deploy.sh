89d06f873bc158939f169f0b9d2eb70b660fb201
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-11-04 00:20:44 -0200

    feat(deploy): Creating a deploy script
    This commit adds the script that bassically does anything necessary in
    order to deploy the application.
    
    As you can see Im doing some tricks at pre-deploy.sh to replace some
    strings that indicate assets location to my current production host..
    
    I know this is not the best way to do it (it breaks my heart 💔), but I
    really wanna see it in production 😝

---
+++ pre-deploy.sh
@@ -0,0 +1,11 @@
+export HOSTNAME FILENAME BUILD_DIR
+
+HOSTNAME=http://weslleyaraujo.github.io/react-flux-puzzle
+FILENAME=index.html
+BUILD_DIR=dist
+
+cp $FILENAME $BUILD_DIR
+./node_modules/replace/bin/replace.js \/$BUILD_DIR $HOSTNAME $BUILD_DIR/$FILENAME
+./node_modules/replace/bin/replace.js \/$BUILD_DIR $HOSTNAME $BUILD_DIR/assets/css/main.css
+./node_modules/replace/bin/replace.js \/$BUILD_DIR $HOSTNAME $BUILD_DIR/assets/js/index.js
+
