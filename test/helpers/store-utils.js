382311d65051520134d1a0c92c3e8b4e0bdfc9f0
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-11-07 15:14:21 -0200

    feat(specs): Setting up a spec runner
    I was felling the need to create some specs for the app..

---
+++ test/helpers/store-utils.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Store utiles', () => {
+  describe('getSchema', () => {
+    it('should return the store schema', () => {
+      expect(1).to.be.a('number');
+    });
+  });
+
+});
+
