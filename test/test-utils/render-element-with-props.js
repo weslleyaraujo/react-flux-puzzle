0a3652a47e08c4e0a1522d3bae2799337531d575
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-11-13 11:47:54 -0200

    feat(specs): Exporting shallow output
    Also exporting shallow output in renderElementWithProps cause it will be
    usefull in some spec cases.

--- test/test-utils/render-element-with-props.js
+++ test/test-utils/render-element-with-props.js
@@ -3,9 +3,11 @@ import sd from 'skin-deep';
 export default function renderElementWithProps(component) {
   let element = sd.shallowRender(component);
   let instance = element.getMountedInstance();
+  let output = element.getRenderOutput();
 
   return {
     element,
-    instance
+    instance,
+    output
   }
 }
